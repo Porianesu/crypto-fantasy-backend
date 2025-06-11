80080f52a7a22c5fab5b60a8337b645a1cfc267c
Porianesu <976384615@qq.com>
2025-06-11 19:28:21 +0800

    Update:1.update schema.prisma

--- api/users.ts
+++ api/users.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'GET') {
     try {
       const users = await prisma.user.findMany();
