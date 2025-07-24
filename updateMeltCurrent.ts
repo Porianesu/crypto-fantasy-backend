967cd7eb10960a16b07d17fffdc3884a794c612e
Porianesu <976384615@qq.com>
2025-07-24 18:32:19 +0800

    Add scheduled workflow to update meltCurrent for users

---
+++ updateMeltCurrent.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function updateMeltCurrent() {
+  // 只更新 meltCurrent < meltMax 的用户
+  await prisma.user.updateMany({
+    where: {
+      meltCurrent: { lt: prisma.user.fields.meltMax }
+    },
+    data: {
+      meltCurrent: {
+        increment: 1
+      }
+    }
+  })
+  await prisma.$disconnect()
+}
+
+updateMeltCurrent().then(() => {
+  console.log('MeltCurrent updated for all users.')
+}).catch((err) => {
+  console.error('Error updating MeltCurrent:', err)
+  process.exit(1)
+})
