f487dfcb255a71d5fe827bb0fef4bdccffeb6282
Porianesu <976384615@qq.com>
2025-07-24 17:24:20 +0800

    Update: include addedFaithCoin in response for card melting in melt-card.ts

--- api/melt-card.ts
+++ api/melt-card.ts
@@ -64,6 +64,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   });
 
   return res.status(200).json({
+    addedFaithCoin: meltConfig.faithCoin,
     faithAmount: updatedUser.faithAmount,
   });
 }
