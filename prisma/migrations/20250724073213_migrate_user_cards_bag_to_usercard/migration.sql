1bc3a8ac2b133f82b9f6768344274f9721ea5a12
Porianesu <976384615@qq.com>
2025-07-24 15:32:26 +0800

    Update: create UserCard table and establish foreign key relationships with User and Card tables

---
+++ prisma/migrations/20250724073213_migrate_user_cards_bag_to_usercard/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "UserCard" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "cardId" INTEGER NOT NULL,
+    "obtainedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserCard_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "UserCard" ADD CONSTRAINT "UserCard_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserCard" ADD CONSTRAINT "UserCard_cardId_fkey" FOREIGN KEY ("cardId") REFERENCES "Card"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
