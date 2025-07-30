31ff1cfa1d87e8e1e78715a19432935aba71c502
Porianesu <976384615@qq.com>
2025-07-30 15:05:32 +0800

    Update: refactor ESLint configuration to use new parser and plugins for improved TypeScript support

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,29 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
-  env: {
-    node: true,
-    es2021: true
+const js = require('@eslint/js');
+const globals = require('globals');
+const tsEslint = require('typescript-eslint');
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = tsEslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tsEslint.configs.recommended,
+      prettierConfig,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off'
+    },
   },
-  rules: {
-    'prettier/prettier': 'error'
-  }
-};
+)
