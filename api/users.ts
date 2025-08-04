11533e19280a8acea2ecf6d7c8ea2c6abc4bac91
Porianesu <976384615@qq.com>
2025-08-04 19:55:54 +0800

    Update: add PATCH method support in CORS headers for user updates

--- api/users.ts
+++ api/users.ts
@@ -9,7 +9,7 @@ const prisma = new PrismaClient()
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // 设置CORS头
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PATCH')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
 
   if (req.method === 'OPTIONS') {
