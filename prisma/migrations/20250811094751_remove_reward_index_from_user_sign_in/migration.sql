70394a73f339de80b6e6e05caaa1368bef38f144
Porianesu <976384615@qq.com>
2025-08-11 17:50:03 +0800

    Update: drop rewardIndex column from UserSignIn table

---
+++ prisma/migrations/20250811094751_remove_reward_index_from_user_sign_in/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rewardIndex` on the `UserSignIn` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserSignIn" DROP COLUMN "rewardIndex";
